bb944b2d42344adae356b72d9fe2927524997c7a
kalmarek <kalmar@amu.edu.pl>
2019-08-06 04:36:14 +0200

    fix a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,17 @@ using LightGraphs
 
 @testset "GL₂" begin
     @testset "PGL₂" begin
-        @test_throws AssertionError PGL₂{5}(-4, 2 , 6, 2)
+        @test_throws ArgumentError PGL₂{5}(-4, 2 , 6, 2)
         m = PGL₂{5}(-4, 2 , 6, 4)
         @test m == PGL₂{5}(1, 2, 1, 4)
         @test det(m) == 2
-        @test invmod(det(m), 5) == 3
+        @test det(m) isa RamanujanGraphs.IntMod{5}
+        @test inv(det(m)) == 3
         @test m*m == PGL₂{5}(3, 0, 0, 3)
         @test m^2 == PGL₂{5}(1, 0, 0, 1)
         @test inv(m) * m == PGL₂{5}(1, 0, 0, 1)
 
+        # testing correct indexing
         m = PGL₂{5}([0 3; 1 4])
         @test m[1] == 0
         @test m[2] == 1
@@ -47,40 +49,53 @@ end
     @test length(RamanujanGraphs.quadruples_4k_plus3(7)) == 8
     @test length(RamanujanGraphs.quadruples_4k_plus3(11)) == 12
     @test length(RamanujanGraphs.quadruples_4k_plus3(19)) == 20
+
+    for p in [3,5,7,11,13,17,19,23]
+        @test length(RamanujanGraphs.quadruples(p)) == p+1
+    end
 end
 
 @testset "lpsgenerators" begin
 
-    @test lps_generators(3, 5) isa Vector{PGL₂{5}}
-    @test lps_generators(7, 5) isa Vector{PGL₂{5}}
-    @test lps_generators(11, 5) isa Vector{PSL₂{5}}
+    @test lps_generators(13, 5) isa Vector{PGL₂{5}}
+    @test lps_generators(17, 13) isa Vector{PSL₂{13}}
 
-    @test length(lps_generators(3, 5)) == 4
-    @test length(lps_generators(7, 5)) == 8
-    @test length(lps_generators(11, 5)) == 12
-
-
-    for p in [3,7,11]
-        S = lps_generators(p, 5);
-        E, sizes = RamanujanGraphs.generate_balls(S, radius=5);
-        @test all(isequal(p+1), (length(unique(g*s for s in S)) for g in E))
+    for p in [5, 13, 17, 29, 37]
+        for q in [13, 17, 29, 37]
+            if p ≠ q
+                @test length(lps_generators(p, q)) == p+1
+            end
+        end
     end
-    # S = lps_generators(7, 5);
-    # E, sizes = generate_balls(S, radius=4);
-    # @test all(isequal(8), (length(unique(g*s for s in S)) for g in E))
-    #
-    # S = lps_generators(11, 5);
-    # E, sizes = generate_balls(S, radius=4);
-    # @test all(isequal(12), (length(unique(g*s for s in S)) for g in E))
+
+    for p in [5, 13, 17, 29, 37]
+        for q in [13, 17]
+            if p ≠ q
+                S = lps_generators(p, q);
+                E, sizes = RamanujanGraphs.generate_balls(S, radius= 2p)
+                @test sizes[end] == RamanujanGraphs.order(eltype(S))
+                @test all(isequal(p+1), (length(unique(g*s for s in S)) for g in E))
+            end
+        end
+    end
 end
 
 
 @testset "Cayley graphs" begin
-    let (p, q) = (3, 5)
+    let (p, q) = (13, 5)
         S = lps_generators(p, q)
-        c, vertices, vlabels = cayley_graph(S, radius=6)
+        c, vertices, vlabels = cayley_graph(RamanujanGraphs.reduced_generating_set(S), radius=6)
         @test length(vertices) == RamanujanGraphs.order(eltype(vertices))
-        @test all(isequal(p+1), degree(c))
+        @test all(isequal(p+1), LightGraphs.degree(c))
+        @test all(v in keys(vlabels) for v in vertices)
+        @test all(vlabels[vertices[i]] == i for (g,i) in vlabels)
+    end
+
+    let (p, q) = (37, 29)
+        S = lps_generators(p, q)
+        c, vertices, vlabels = cayley_graph(RamanujanGraphs.reduced_generating_set(S), radius=6)
+        @test length(vertices) == RamanujanGraphs.order(eltype(vertices))
+        @test all(isequal(p+1), LightGraphs.degree(c))
         @test all(v in keys(vlabels) for v in vertices)
         @test all(vlabels[vertices[i]] == i for (g,i) in vlabels)
     end
