0411369e991e2ef5f1ff3185f8dbdacd4a7b76d6
kalmarek <kalmar@amu.edu.pl>
2020-05-08 23:48:52 +0200

    move LPS-related stuff

--- src/RamanujanGraphs.jl
+++ src/RamanujanGraphs.jl
@@ -4,32 +4,13 @@ using Primes
 using LinearAlgebra
 using LightGraphs
 
-export PGL₂, PSL₂, lps_generators, cayley_graph, lps
+export lps_generators, cayley_graph, lps
+export GL₂, SL₂, PGL₂, PSL₂, Borel, Bruhat, bruhat
 
 include("cayley.jl")
 include("intmod.jl")
 include("gl2.jl")
+
 include("lps_generators.jl")
-
-function lps(p::Integer, q::Integer)
-    S = lps_generators(p,q)
-    G, verts, vlabels, elabels = cayley_graph(order(eltype(S)), S)
-    @assert order(eltype(S)) == length(verts)
-    @assert all(isequal(p+1), degree(G))
-    return G, verts, vlabels, elabels
-end
-
-function lps(p::Integer, q::Integer, radius::Integer)
-    S = lps_generators(p,q)
-    G, verts, vlabels, elabels = cayley_graph(S, radius=radius)
-    radius < diameter_ub(p,q) && @warn "Radius given is smaller than its upper bound, cayley graph might be not complete!"
-    return G, verts, labels, elabels
-end
-
-# from Lubotzky-Phillips-Sarnak
-function diameter_ub(p::Integer, q::Integer)
-    n = order(PGLtype(p,q))
-    return floor(Int, 2log(p, n) + 2log(p,2) + 1)
-end
 
 end # module
--- src/lps_generators.jl
+++ src/lps_generators.jl
@@ -89,3 +89,24 @@ function lps_generators(p::Integer, q::Integer)
 	@assert all((!isone).(S))
     return S
 end
+
+function lps(p::Integer, q::Integer)
+    S = lps_generators(p,q)
+    G, verts, vlabels, elabels = cayley_graph(order(eltype(S)), S)
+    @assert order(eltype(S)) == length(verts)
+    @assert all(isequal(p+1), degree(G))
+    return G, verts, vlabels, elabels
+end
+
+function lps(p::Integer, q::Integer, radius::Integer)
+    S = lps_generators(p,q)
+    G, verts, vlabels, elabels = cayley_graph(S, radius=radius)
+    radius < diameter_ub(p,q) && @warn "Radius given is smaller than its upper bound, cayley graph might be not complete!"
+    return G, verts, labels, elabels
+end
+
+# from Lubotzky-Phillips-Sarnak
+function diameter_ub(p::Integer, q::Integer)
+    n = order(PGLtype(p,q))
+    return floor(Int, 2log(p, n) + 2log(p,2) + 1)
+end
